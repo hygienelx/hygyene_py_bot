ef79fbbcb7257cec33963ccec08d72a18d0f0ca3
Tomás <agu99tomas@gmail.com>
2022-07-30 19:48:43 -0300

    Update main.py

--- hygiene_py_bot/main.py
+++ hygiene_py_bot/main.py
@@ -1,7 +1,7 @@
 import schedule
 import time
 from teams_messages import daily
-from hygiene_py_bot.consts import QUARTER_TO_NINE
+from consts import QUARTER_TO_NINE
 
 schedule.every().monday.at(QUARTER_TO_NINE).do(daily.send)
 schedule.every().tuesday.at(QUARTER_TO_NINE).do(daily.send)
