95e263a2858e1103fee20aeb0ff24f0592a23a39
Tomás <agu99tomas@gmail.com>
2022-07-30 19:15:35 -0300

    utc

--- hygiene_py_bot/main.py
+++ hygiene_py_bot/main.py
@@ -1,12 +1,15 @@
 import schedule
 import time
 from teams_messages import daily
+from hygiene_py_bot.consts import QUARTER_TO_NINE
 
-schedule.every().monday.at("08:45").do(daily.send)
-schedule.every().tuesday.at("08:45").do(daily.send)
-schedule.every().wednesday.at("08:45").do(daily.send)
-schedule.every().thursday.at("08:45").do(daily.send)
-schedule.every().friday.at("08:45").do(daily.send)
+schedule.every().monday.at(QUARTER_TO_NINE).do(daily.send)
+schedule.every().tuesday.at(QUARTER_TO_NINE).do(daily.send)
+schedule.every().wednesday.at(QUARTER_TO_NINE).do(daily.send)
+schedule.every().thursday.at(QUARTER_TO_NINE).do(daily.send)
+schedule.every().friday.at(QUARTER_TO_NINE).do(daily.send)
+
+schedule.every().saturday.at("22:20").do(daily.send)
 
 while True:
     schedule.run_pending()
