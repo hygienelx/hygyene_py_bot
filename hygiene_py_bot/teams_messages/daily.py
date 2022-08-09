a6f5b11379ba5ddb900d8ac38866043d94465263
Tomás <agu99tomas@gmail.com>
2022-08-08 23:12:18 -0300

    emi

--- hygiene_py_bot/teams_messages/daily.py
+++ hygiene_py_bot/teams_messages/daily.py
@@ -13,7 +13,7 @@ def send_daily_message(hook_url):
     message.send()
 
 def get_text():
-    team_members = ["Juanfa", "Clau", "Alex", "Santi", "Tomi"]
+    team_members = ["Juanfa", "Clau", "Alex", "Santi", "Tomi", "Emi"]
     random.shuffle(team_members)
 
     text_list = [ f"{i+1}) {m}" for i, m in enumerate(team_members)]
