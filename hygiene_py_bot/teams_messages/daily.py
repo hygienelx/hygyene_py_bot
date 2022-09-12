bf24da4e19037191208f61542c915da189951103
agutomas <111798207+agutomas@users.noreply.github.com>
2022-09-12 11:12:53 -0300

    Update daily.py

--- hygiene_py_bot/teams_messages/daily.py
+++ hygiene_py_bot/teams_messages/daily.py
@@ -13,7 +13,7 @@ def send_daily_message(hook_url):
     message.send()
 
 def get_text():
-    team_members = ["Anto", "Clau", "Alex", "Santi", "Tomi", "Emi"]
+    team_members = ["Anto", "Clau", "Alex", "Santi", "Tomi", "Emi", "Juangui"]
     random.shuffle(team_members)
 
     text_list = [ f"{i+1}) {m}" for i, m in enumerate(team_members)]
